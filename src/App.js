676e2ac03313d3bf8eb119269cecd85b033c405c
Tal Barzilay <talbarzi@post.bgu.ac.il>
2021-11-08 10:48:47 +0200

    added routes to project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import React  from 'react';
+
 import './App.css';
 
 import Signin from './components/sign-in/sign-in.component'
 
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
-        <Signin></Signin>
-    </div>
+    <Router>
+      <div className="App">
+       <Routes>
+          <Route path='/signin' element={<Signin />} />
+       </Routes>
+      </div>
+    </Router>
   );
 }
 
